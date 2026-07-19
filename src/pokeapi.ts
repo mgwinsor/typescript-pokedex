c7ded7642b64aef96ee1bc5aa05535bc0f38523a
Michael Winsor <mgwinsor@gmail.com>
2026-07-19 23:05:09 +0800

    add: cache

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -1,10 +1,20 @@
+import { Cache } from "./pokecache.js";
+
 export class PokeAPI {
   private static readonly baseURL = "https://pokeapi.co/api/v2";
+  private cache: Cache;
 
-  constructor() {}
+  constructor(cacheInterval: number) {
+    this.cache = new Cache(cacheInterval);
+  }
 
   async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
     const url = pageURL || `${PokeAPI.baseURL}/location-area`;
+
+    const cached = this.cache.get<ShallowLocations>(url);
+    if (cached) {
+      return cached;
+    }
 
     try {
       const resp = await fetch(url);
@@ -14,6 +24,7 @@ export class PokeAPI {
       }
 
       const locations: ShallowLocations = await resp.json();
+      this.cache.add(url, locations);
       return locations;
     } catch (e) {
       throw new Error(`Error fetching locations ${(e as Error).message}`);
@@ -23,6 +34,11 @@ export class PokeAPI {
   async fetchLocation(locationName: string): Promise<Location> {
     const url = `${PokeAPI.baseURL}/location-area/${locationName}`;
 
+    const cached = this.cache.get<Location>(url);
+    if (cached) {
+      return cached;
+    }
+
     try {
       const resp = await fetch(url);
 
@@ -31,6 +47,7 @@ export class PokeAPI {
       }
 
       const location: Location = await resp.json();
+      this.cache.add(url, location);
       return location;
     } catch (e) {
       throw new Error(
---
+++ src/pokecache.ts
@@ -0,0 +1,53 @@
+type CacheEntry<T> = {
+  createdAt: number;
+  val: T;
+};
+
+export class Cache {
+  #cache = new Map<string, CacheEntry<any>>();
+  #reapIntervalId: NodeJS.Timeout | undefined = undefined;
+  #interval: number;
+
+  constructor(interval: number) {
+    this.#interval = interval;
+    this.#startReapLoop();
+  }
+
+  add<T>(key: string, val: T) {
+    const cacheEntry: CacheEntry<T> = {
+      createdAt: Date.now(),
+      val: val,
+    };
+    this.#cache.set(key, cacheEntry);
+  }
+
+  get<T>(key: string): T | undefined {
+    const entry = this.#cache.get(key);
+    if (entry !== undefined) {
+      return entry.val as T;
+    }
+    return undefined;
+  }
+
+  #reap() {
+    const now = Date.now();
+    for (const [key, entry] of this.#cache) {
+      if (now - entry.createdAt > this.#interval) {
+        this.#cache.delete(key);
+      }
+    }
+  }
+
+  #startReapLoop() {
+    this.#reapIntervalId = setInterval(() => {
+      this.#reap();
+    }, this.#interval);
+  }
+
+  stopReapLoop() {
+    if (this.#reapIntervalId) {
+      clearInterval(this.#reapIntervalId);
+      this.#reapIntervalId = undefined;
+    }
+  }
+}
